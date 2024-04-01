33eadc0a5871153cde80e970e2a5c56a9b4fc083
fazekas <fazekasxluca98@gmail.com>
2024-04-01 17:06:31 +0200

    feladat2 modositva

--- index.js
+++ index.js
@@ -36,3 +36,39 @@ function KeruletTerulet(sugar) {
 var sugar1 = 5;
 var eredmeny2 = KeruletTerulet(sugar1);
 console.log(eredmeny2);
+/*
+-Keszits fgv-t , ami kivalasztja egy auto tombbol a legkisebb heng.ur.tartalmu autot
+-Keszits fgv-t, ami megadja a parameterul kapott auto tombbol a benzinesek db számát
+-A valtozasokat tolts fel a git hub repoba
+*/
+//-Keszits fgv-t , ami kivalasztja egy auto tombbol a legkisebb heng.ur.tartalmu autot
+function MinHenUrtart(autoT) {
+    var min = autoT[0];
+    for (var i = 0; i < autoT.length; i++) {
+        if (min.hengerurtartalom > autoT[i].hengerurtartalom) {
+            min = autoT[i];
+        }
+    }
+    return min;
+}
+var autoObj1 = {
+    gyarto: "Opel",
+    tipus: "Corsa",
+    hengerurtartalom: 2400,
+    benzines: true,
+};
+var autoObj2 = {
+    gyarto: "Opel",
+    tipus: "Corsa",
+    hengerurtartalom: 1400,
+    benzines: true,
+};
+var autoObj3 = {
+    gyarto: "Opel",
+    tipus: "Corsa",
+    hengerurtartalom: 3450,
+    benzines: false,
+};
+var t = [autoObj1, autoObj2, autoObj3];
+var eredmeny3 = MinHenUrtart(t);
+console.log(eredmeny3);
--- index.ts
+++ index.ts
@@ -66,5 +66,54 @@ interface Auto{
     gyarto:string,
     tipus:string,
     hengerurtartalom:number,
-    beniznes:boolean
+    benzines:boolean
 }
+
+/*
+-Keszits fgv-t , ami kivalasztja egy auto tombbol a legkisebb heng.ur.tartalmu autot
+-Keszits fgv-t, ami megadja a parameterul kapott auto tombbol a benzinesek db számát
+-A valtozasokat tolts fel a git hub repoba
+*/
+
+//-Keszits fgv-t , ami kivalasztja egy auto tombbol a legkisebb heng.ur.tartalmu autot
+
+
+function MinHenUrtart(autoT:Auto[]):Auto
+{
+
+    var min:Auto = autoT[0];
+
+    for(var i:number  =0; i < autoT.length;i++)
+    {
+      if(min.hengerurtartalom  > autoT[i].hengerurtartalom)
+      {
+        min = autoT[i];
+      }
+
+    }
+
+    return min;
+}
+var autoObj1 = {
+    gyarto:"Opel",
+    tipus:"Corsa",
+    hengerurtartalom:2400,
+    benzines:true,
+};
+var autoObj2 = {
+    gyarto:"Opel",
+    tipus:"Corsa",
+    hengerurtartalom:1400,
+    benzines:true,
+};
+var autoObj3 = {
+    gyarto:"Opel",
+    tipus:"Corsa",
+    hengerurtartalom:3450,
+    benzines:false,
+};
+
+var t:Auto[] = [autoObj1,autoObj2,autoObj3];
+var eredmeny3 = MinHenUrtart(t);
+console.log(eredmeny3);
+
